90cb63cb0016b3dff43aa1165c145ea9fe62f2c3
Thomas Kjærulff <thomas.kjaerulff@fellowmind.dk>
2026-01-27 20:39:41 +0100

    Add text about Azure Web Apps and GitHub Actions

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,9 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p className="read-the-docs">
+        Hopefully this will deploy to Azure Web Apps using GitHub Actions!
+      </p>
     </>
   )
 }
